6405c19df809bee4091afe58d17a06c258cc6a8a
khanq3 <2021wc86573@wilp.bits-pilani.ac.in>
2025-06-14 04:13:00 -0500

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.quamran;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class CodeInspectionApplication {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(CodeInspectionApplication.class, args);
+	}
+ 
 }
